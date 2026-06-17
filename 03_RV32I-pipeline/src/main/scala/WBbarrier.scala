f95a3626ef9d84985a6f0539c1b0ad0fca9f92df
felucio <qyx78hyt@rptu.de>
2026-06-17 16:07:25 +0200

    WBbarrier done

--- 03_RV32I-pipeline/src/main/scala/WBbarrier.scala
+++ 03_RV32I-pipeline/src/main/scala/WBbarrier.scala
@@ -33,3 +33,22 @@ import chisel3._
 // -----------------------------------------
 
 //ToDo: Add your implementation according to the specification above here 
+
+class WBbarrier extends Module{
+  val io = IO(new Bundle {
+    val inCheckRes = Input(UInt(32.W))
+    val inXcptInvalid = Input(Bool())
+
+    val outCheckRes = Output(UInt(32.W))
+    val outXcptInvalid = Output(Bool())
+  })
+
+  val check_res = RegInit(0.U(32.W))
+  val isInvalid = RegInit(false.B)
+
+  check_res := io.inCheckRes
+  isInvalid := io.inXcptInvalid
+
+  io.outCheckRes := check_res
+  io.outXcptInvalid := isInvalid
+}
