8a0ea0e47db025fae8cdfa480ce28365e334963f
felucio <qyx78hyt@rptu.de>
2026-07-14 10:52:25 +0200

    Transferred previous test files

--- 06_Branch-Target-Buffer/src/test/scala/PipelinedRISCV32I_tb.scala
+++ 06_Branch-Target-Buffer/src/test/scala/PipelinedRISCV32I_tb.scala
@@ -15,66 +15,163 @@ class PipelinedRISCV32ITest extends AnyFlatSpec with ChiselScalatestTester {
 
 "RV32I_BasicTester" should "work" in {
     test(new PipelinedRV32I("src/test/programs/BinaryFile_pipelined")).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
-
       dut.clock.setTimeout(0)
       dut.clock.step(5)
-      dut.io.result.expect(0.U)     // ADDI x0, x0, 0
+
+      dut.io.result.expect(4.U)
       dut.io.exception.expect(false.B)
+      println("=================================================")
+      println("--FROM WB STAGE--")
+      println(f"ADDI x3, x0, 4: 0x${dut.io.result.peek().litValue}%08X")
+      println(f"PCSrcE: 0x${dut.io.PCSrcE_debug.peek().litValue}%08X")
+      println("=================================================")
+
       dut.clock.step(1)
-      dut.io.result.expect(4.U)     // ADDI x1, x0, 4
+
+      dut.io.result.expect(8.U)
       dut.io.exception.expect(false.B)
+      println("--FROM WB STAGE--")
+      println(f"ADDI x4, x0, 8: 0x${dut.io.result.peek().litValue}%08X")
+      println(f"PCSrcE: 0x${dut.io.PCSrcE_debug.peek().litValue}%08X")
+      println("=================================================")
+
       dut.clock.step(1)
-      dut.io.result.expect(5.U)     // ADDI x2, x0, 5
+
+      //dut.io.result.expect(6.U)
       dut.io.exception.expect(false.B)
+      println("-- CYCLE --")
+      println(f"ADDI x3, x3, 1: 0x${dut.io.result.peek().litValue}%08X")
+      println(f"PCSrcE: 0x${dut.io.PCSrcE_debug.peek().litValue}%08X")
+      println("=================================================")
       dut.clock.step(1)
-      dut.io.result.expect(9.U)     // ADD x3, x1, x2
+
+      //==========================================================
+
       dut.io.exception.expect(false.B)
+      println("-- BNE --")
+      println(f"BNE  x4, x3, -4: 0x${dut.io.result.peek().litValue}%08X")
+      println(f"PCSrcE: 0x${dut.io.PCSrcE_debug.peek().litValue}%08X")
+      println("=================================================")
+      dut.clock.step(3)
+
+      dut.io.exception.expect(false.B)
+      println("--2 CYCLES AFTER--")
+      println(f"SHOULD CYCLE: 0x${dut.io.result.peek().litValue}%08X")
+      println(f"PCSrcE: 0x${dut.io.PCSrcE_debug.peek().litValue}%08X")
+      println("=================================================")
       dut.clock.step(1)
-      dut.io.result.expect(2047.U)  // ADDI x4, x0, 2047
+
       dut.io.exception.expect(false.B)
+      println("-- BNE --")
+      println(f"SHOULD CYCLE: 0x${dut.io.result.peek().litValue}%08X")
+      println(f"PCSrcE: 0x${dut.io.PCSrcE_debug.peek().litValue}%08X")
+      println("=================================================")
+      dut.clock.step(3)
+
+      dut.io.exception.expect(false.B)
+      println("--2 CYCLES AFTER--")
+      println(f"SHOULD CYCLE: 0x${dut.io.result.peek().litValue}%08X")
+      println(f"PCSrcE: 0x${dut.io.PCSrcE_debug.peek().litValue}%08X")
+      println("=================================================")
       dut.clock.step(1)
-      dut.io.result.expect(16.U)    // ADDI x5, x0, 16
+
       dut.io.exception.expect(false.B)
+      println("-- BNE -")
+      println(f"SHOULD CYCLE: 0x${dut.io.result.peek().litValue}%08X")
+      println(f"PCSrcE: 0x${dut.io.PCSrcE_debug.peek().litValue}%08X")
+      println("=================================================")
+      dut.clock.step(3)
+
+      dut.io.exception.expect(false.B)
+      println("-- 2 CYCLES AFTER --")
+      println(f"SHOULD CYCLE: 0x${dut.io.result.peek().litValue}%08X")
+      println(f"PCSrcE: 0x${dut.io.PCSrcE_debug.peek().litValue}%08X")
+      println("=================================================")
       dut.clock.step(1)
-      dut.io.result.expect(2031.U)  // SUB x6, x4, x5
+
+      //BNE FINISHED CYCLE
+
       dut.io.exception.expect(false.B)
+      println("-- BNE --")
+      println(f"SHOULD EXIT CYCLE: 0x${dut.io.result.peek().litValue}%08X")
+      println(f"PCSrcE: 0x${dut.io.PCSrcE_debug.peek().litValue}%08X")
+      println("=================================================")
       dut.clock.step(1)
-      dut.io.result.expect(2022.U)  // XOR x7, x6, x3
+
       dut.io.exception.expect(false.B)
+      println("-- ADDI x5, x0, 16 --")
+      println(f"ADDI x5, x0, 16: 0x${dut.io.result.peek().litValue}%08X")
+      println(f"PCSrcE: 0x${dut.io.PCSrcE_debug.peek().litValue}%08X")
+      println("=================================================")
       dut.clock.step(1)
-      dut.io.result.expect(2047.U)  // OR x8, x6, x5
+
+      dut.io.result.expect(24.U)
       dut.io.exception.expect(false.B)
+      println("-- ADD x5, x5, x4 --")
+      println(f"ADD x5, x5, x4: 0x${dut.io.result.peek().litValue}%08X")
+      println(f"PCSrcE: 0x${dut.io.PCSrcE_debug.peek().litValue}%08X")
+      println("=================================================")
       dut.clock.step(1)
-      dut.io.result.expect(0.U)     // AND x9, x6, x5
+
+      dut.io.result.expect(12.U)
       dut.io.exception.expect(false.B)
+      println("-- SRAI x5, x5, 1 --")
+      println(f"SRAI x5, x5, 1: 0x${dut.io.result.peek().litValue}%08X")
+      println(f"PCSrcE: 0x${dut.io.PCSrcE_debug.peek().litValue}%08X")
+      println("=================================================")
       dut.clock.step(1)
-      dut.io.result.expect(64704.U) // SLL x10, x7, x2
+
       dut.io.exception.expect(false.B)
+      println("-- JAL x3, 8 --")
+      println(f"JAL x3, 8: 0x${dut.io.result.peek().litValue}%08X")
+      println(f"PCSrcE: 0x${dut.io.PCSrcE_debug.peek().litValue}%08X")
+      println("=================================================")
+      dut.clock.step(3)
+
+      dut.io.exception.expect(false.B)
+      dut.io.result.expect(12.U)
+      println("-- AFTER 2 CYCLES --")
+      println(f"ADDI x5, x5, 0: 0x${dut.io.result.peek().litValue}%08X")
+      println(f"PCSrcE: 0x${dut.io.PCSrcE_debug.peek().litValue}%08X")
+      println("=================================================")
       dut.clock.step(1)
-      dut.io.result.expect(63.U)    // SRL x11, x7, x2
+
       dut.io.exception.expect(false.B)
+      println("-- ADDI x3, x3, 0 --")
+      println(f"ADDI x3, x3, 0: 0x${dut.io.result.peek().litValue}%08X")
+      println(f"PCSrcE: 0x${dut.io.PCSrcE_debug.peek().litValue}%08X")
+      println("=================================================")
       dut.clock.step(1)
-      dut.io.result.expect(63.U)    // SRA x12, x7, x2
+
       dut.io.exception.expect(false.B)
+      println("-- JALR x7, 0(x3) --")
+      println(f"JALR x7, 0(x3): 0x${dut.io.result.peek().litValue}%08X")
+      println(f"PCSrcE: 0x${dut.io.PCSrcE_debug.peek().litValue}%08X")
+      println("=================================================")
+      dut.clock.step(3)
+
+      dut.io.exception.expect(false.B)
+      println("-- 2 CYCLES AFTER --")
+      println(f"ADDI x5, x0, 3: 0x${dut.io.result.peek().litValue}%08X")
+      println(f"PCSrcE: 0x${dut.io.PCSrcE_debug.peek().litValue}%08X")
+      println("=================================================")
       dut.clock.step(1)
-      dut.io.result.expect(0.U)     // SLT x13, x4, x4
+
       dut.io.exception.expect(false.B)
+      println("-- ADDI x5, x5, 0 --")
+      println(f"ADDI x5, x5, 0: 0x${dut.io.result.peek().litValue}%08X")
+      println(f"PCSrcE: 0x${dut.io.PCSrcE_debug.peek().litValue}%08X")
+      println("=================================================")
       dut.clock.step(1)
-      dut.io.result.expect(0.U)     // SLT x13, x4, x5
-      dut.io.exception.expect(false.B)
-      dut.clock.step(1)
-      dut.io.result.expect(1.U)     // SLT x13, x5, x4
-      dut.io.exception.expect(false.B)
-      dut.clock.step(1)
-      dut.io.result.expect(0.U)     // SLTU x13, x4, x4
-      dut.io.exception.expect(false.B)
-      dut.clock.step(1)
-      dut.io.result.expect(0.U)     // SLTU x13, x4, x5
-      dut.io.exception.expect(false.B)
-      dut.clock.step(1)
-      dut.io.result.expect(1.U)     // SLTU x13, x5, x4
-      dut.io.exception.expect(false.B)
-      dut.clock.step(1)           
     }
   }
 }
+
+/*
+      dut.io.exception.expect(false.B)
+      println("-- DEBUG --")
+      println(f"DEBUG: 0x${dut.io.result.peek().litValue}%08X")
+      println(f"PCSrcE: 0x${dut.io.PCSrcE_debug.peek().litValue}%08X")
+      println("=================================================")
+      dut.clock.step(1)
+ */
